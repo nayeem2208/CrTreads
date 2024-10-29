8997f61d5e7f90cf414ba09a9a65588b257c2199
nayeem2208 <nayeem2281998@gmail.com>
2024-10-29 19:26:22 +0530

    offer images

--- frontend/app/component/Home/OfferComponent.jsx
+++ frontend/app/component/Home/OfferComponent.jsx
@@ -1,6 +1,71 @@
-import React from "react";
+'use client'
+import OfferImages from "@/app/jsFiles/OfferImages";
+import React, { useEffect, useState } from "react";
 
 function OfferComponent() {
+  const [randomOne, setRandomNumberOne] = useState(0);
+  const [randomTwo, setRandomNumberTwo] = useState(0);
+  const [randomThree, setRandomNumberThree] = useState(0);
+  const [randomFour, setRandomNumberFour] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const random = Math.floor(Math.random() * 4);
+      setRandomNumberOne(random);
+    }, 3000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const random = Math.floor(Math.random() * 4);
+      setRandomNumberTwo(random);
+    }, 3000);
+
+    const initialDelay = setTimeout(() => {
+      setRandomNumberTwo(Math.floor(Math.random() * 4));
+    }, 200);
+
+    return () => {
+      clearInterval(intervalId);
+      clearTimeout(initialDelay);
+    };
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const random = Math.floor(Math.random() * 4);
+      setRandomNumberThree(random);
+    }, 3000);
+
+    const initialDelay = setTimeout(() => {
+      setRandomNumberThree(Math.floor(Math.random() * 4));
+    }, 500);
+
+    return () => {
+      clearInterval(intervalId);
+      clearTimeout(initialDelay);
+    };
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const random = Math.floor(Math.random() * 4);
+      setRandomNumberFour(random);
+    }, 3000);
+
+    const initialDelay = setTimeout(() => {
+      setRandomNumberFour(Math.floor(Math.random() * 4));
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+      clearTimeout(initialDelay);
+    };
+  }, []);
   return (
     <div className="flex justify-center bg-stone-50 shadow-inner items-center h-full pt-12  lg:pt-20">
       <div className="w-5/6 md:w-4/6">
@@ -9,7 +74,7 @@ function OfferComponent() {
           <div className="w-full lg:w-1/4 mt-4 lg:mt-0 lg:mr-3">
             <div className="h-48 w-full overflow-hidden">
               <img
-                src="https://i.pinimg.com/originals/29/a8/e5/29a8e57b8b799151ffdf9bc7ceca9db2.jpg"
+                src={OfferImages[0][randomOne]}
                 alt=""
                 className="h-full w-full object-cover"
               />
@@ -18,7 +83,7 @@ function OfferComponent() {
           <div className="w-full lg:w-1/4 mt-4 lg:mt-0 lg:mx-3">
             <div className="h-48 w-full overflow-hidden">
               <img
-                src="https://plus.unsplash.com/premium_photo-1675032314301-400d9898183d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src={OfferImages[1][randomTwo]}
                 alt=""
                 className="h-full w-full object-cover"
               />
@@ -27,7 +92,7 @@ function OfferComponent() {
           <div className="w-full lg:w-1/4 mt-4 lg:mt-0 lg:mx-3">
             <div className="h-48 w-full overflow-hidden">
               <img
-                src="https://plus.unsplash.com/premium_photo-1673208484517-deeab27359b4?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src={OfferImages[2][randomThree]}
                 alt=""
                 className="h-full w-full object-cover"
               />
@@ -36,7 +101,7 @@ function OfferComponent() {
           <div className="w-full lg:w-1/4 mt-4 lg:mt-0 lg:ml-3">
             <div className="h-48 w-full overflow-hidden">
               <img
-                src="https://plus.unsplash.com/premium_photo-1664298128120-d2284204dbda?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src={OfferImages[3][randomFour]}
                 alt=""
                 className="h-full w-full object-cover"
               />
