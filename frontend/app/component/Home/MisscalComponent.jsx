501417afdf1d297b612e60a7032dc8a65927e315
nayeem2208 <nayeem2281998@gmail.com>
2024-11-12 15:43:59 +0530

    edited misscall button

--- frontend/app/component/Home/MisscalComponent.jsx
+++ frontend/app/component/Home/MisscalComponent.jsx
@@ -1,31 +1,15 @@
 "use client";
 import React from "react";
-import { FaPhone } from "react-icons/fa6";
-import ReactCurvedText from "react-curved-text";
 
 function MisscalComponent() {
   return (
     <div className="md:hidden">
       <a
         href="tel:+1234567890"
-        className="fixed bottom-4 right-4 bg-red-500 hover:bg-red-600 text-white rounded-full p-4 shadow-lg flex items-center justify-center"
+        className="fixed bottom-4 right-4 w-20 h-20 p-0 bg-transparent rounded-full shadow-lg overflow-hidden flex items-center justify-center"
         style={{ zIndex: 1000 }}
       >
-        {/* <ReactCurvedText
-          width={30}
-          height={30}
-          cx={54}
-          cy={54}
-          rx={100}
-          ry={100}
-          startOffset={0}
-          reversed={true}
-          text="Missed Call"
-          textProps={{ style: { fontSize: 10 } }}
-        /> */}
-        
-          <FaPhone size={24} className="text-white" />
-        
+        <img src="/image.png" alt="Missed Call" className="m-0 w-full h-full object-cover " />
       </a>
     </div>
   );
