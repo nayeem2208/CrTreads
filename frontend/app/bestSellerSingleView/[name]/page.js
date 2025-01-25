d83e9c56acab3a8d09f5c9b73197f249bb54e73d
nayeem2208 <nayeem2281998@gmail.com>
2025-01-25 19:37:24 +0530

    go back button

--- frontend/app/bestSellerSingleView/[name]/page.js
+++ frontend/app/bestSellerSingleView/[name]/page.js
@@ -2,20 +2,30 @@
 import bestSeller from "@/app/jsFiles/BestSeller";
 import { useParams } from "next/navigation";
 import React from "react";
+import { MdKeyboardBackspace } from "react-icons/md";
 
 const BestSellerProductView = () => {
   const { name } = useParams(); // Get the product name from the URL
 
   // Decode the URL parameters
   const decodedName = decodeURIComponent(name);
-  
+
   const selectedProduct = bestSeller.find((product) => product.name === decodedName);
-  
+
   console.log(selectedProduct, 'selected product');
 
   return (
-    <div className="flex justify-center items-center my-10">
-      <div className="w-11/12 md:w-4/6 flex flex-col md:flex-row">
+    <div className="relative flex justify-center items-center lg:my-10">
+      <div className="absolute top-0 left-4 lg:hidden">
+        <div
+          className="p-2 cursor-pointer text-gray-600 flex items-center border rounded-md hover:bg-gray-100"
+          onClick={() => router.back()} // Handle navigation
+        >
+          <MdKeyboardBackspace className="w-5 h-5 font-light mr-2" />
+          <span className="">Go Back</span>
+        </div>
+      </div>
+      <div className="w-11/12 mt-14 md:w-4/6 flex flex-col md:flex-row">
         <div className="p-4 border-2 mr-0 md:mr-8 mb-4 md:mb-0">
           <img src={selectedProduct.imageUrl} alt={selectedProduct.name} className="w-full h-auto md:w-96 md:h-96 object-cover" />
         </div>
