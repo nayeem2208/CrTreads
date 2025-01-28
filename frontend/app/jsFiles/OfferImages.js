50bb9987cef65ce113154e3ae4ac1e9cfef8a5ad
nayeem2208 <nayeem2281998@gmail.com>
2025-01-28 20:56:57 +0530

    new images adding

--- frontend/app/jsFiles/OfferImages.js
+++ frontend/app/jsFiles/OfferImages.js
@@ -1,22 +1,21 @@
-let OfferImages=[[
-    "https://i.pinimg.com/originals/29/a8/e5/29a8e57b8b799151ffdf9bc7ceca9db2.jpg",
-    'https://adzktgbqdq.cloudimg.io/https://dgaddcosprod.blob.core.windows.net/cxf-multisite/clp74il52001611ltajxi2uef/attachments/clqxqbk6g14um11kxes6zfszi-cl1wbn8ma04qn01k5w3ivig3c-r2a4-tyre-tread-depth-and-legal-limit-full.full.webp',
-    'https://emphasysnta.blob.core.windows.net/site-content/michelin-eprimacy_2.jpg',
-    'https://emphasysnta.blob.core.windows.net/site-content/michelin-eprimacy_2.jpg'
+let OfferImages = [[
+    "/CRC5.png",
+    '/CRC3.jpg',
+    '/CRC1.jpg',
+    '/CRC2.jpg'
 ]
-,[
-    "https://plus.unsplash.com/premium_photo-1675032314301-400d9898183d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-'https://d2m3nfprmhqjvd.cloudfront.net/blog/20220228135424/ezgif.com-gif-maker-7.jpg',
-'https://img.freepik.com/premium-photo/dark-storage-full-big-variety-new-tyres-busy-warehouse_926199-3557437.jpg',
-'https://img.freepik.com/premium-photo/dark-storage-full-big-variety-new-tyres-busy-warehouse_926199-338801.jpg'
-]
-,[
+    , [
+    "/CRC4.png",
+    '/CRC3.jpg',
+    '/CRC6.png',
+    '/CRC7.png']
+    , [
     'https://media.drive.com.au/obj/tx_q:70,rs:auto:448:252:1/driveau/upload/cms/uploads/o5m2fsyuhlfk6efilled',
     'https://t3.ftcdn.net/jpg/05/78/48/00/360_F_578480025_RHhNuyTFT8tlyh1uaFDgpKOr4inV7eRt.jpg',
     'https://png.pngtree.com/background/20230522/original/pngtree-three-car-tires-are-in-the-rain-at-night-picture-image_2685650.jpg',
     "https://plus.unsplash.com/premium_photo-1673208484517-deeab27359b4?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 ]
-,[
+    , [
     "https://plus.unsplash.com/premium_photo-1664298128120-d2284204dbda?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     'https://png.pngtree.com/background/20230522/original/pngtree-an-image-of-wheel-and-tire-in-rain-picture-image_2688943.jpg',
     'https://png.pngtree.com/background/20230522/original/pngtree-four-black-tires-line-up-on-the-road-picture-image_2689612.jpg',
