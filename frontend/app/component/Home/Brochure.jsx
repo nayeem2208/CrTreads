18fa3ad8edd76af2ee210881bd66b98132a3a885
nayeem2208 <nayeem2281998@gmail.com>
2025-02-01 00:20:33 +0530

    google translator

--- frontend/app/component/Home/Brochure.jsx
+++ frontend/app/component/Home/Brochure.jsx
@@ -18,12 +18,12 @@ function BrochureDownload() {
         </p>
 
         <a href={brochureUrl} download>
-          <button class="cursor-pointer font-semibold overflow-hidden relative z-100 border border-green-500 group px-8 py-2">
-            <span class="relative z-10 text-green-500 group-hover:text-white text-xl duration-500">
+          <button class="cursor-pointer font-semibold overflow-hidden relative z-100 border border-red-600 group px-8 py-2">
+            <span class="relative z-10 text-red-600 group-hover:text-white text-xl duration-500">
             Download Brochure
             </span>
-            <span class="absolute w-full h-full bg-green-500 -left-42 top-0 -rotate-45 group-hover:rotate-0 group-hover:left-0 duration-500"></span>
-            <span class="absolute w-full h-full bg-green-500 -right-42 top-0 -rotate-45 group-hover:rotate-0 group-hover:right-0 duration-500"></span>
+            <span class="absolute w-full h-full bg-red-600 -left-42 top-0 -rotate-45 group-hover:rotate-0 group-hover:left-0 duration-500"></span>
+            <span class="absolute w-full h-full bg-red-600 -right-42 top-0 -rotate-45 group-hover:rotate-0 group-hover:right-0 duration-500"></span>
           </button>
         </a>
       </div>
